08cc73923b1973ddd3b3c40fecb2ea9efb33324b
Magesh <magesh@atsquare.in>
2013-10-23 19:09:47 +0530

    Initialized the crave product setting up spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crave::Application.config.secret_key_base = 'ea5b5f75d3f8752e800c01fe0eaaab4c860e3dd1aa8a5f66cb60c56a45391fe915819817903460de7768f4b3f5b31d9eadfc1d03a50bb57853d2991ebb70bfdf'

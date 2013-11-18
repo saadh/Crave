8d475ec7b367e72176e9c3c6072c524be34bbcd7
Magesh <magesh@atsquare.in>
2013-11-18 20:04:51 +0530

    Integrated Saad's design for the landing page

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -2,6 +2,7 @@ module Spree
   class HomeController < Spree::StoreController
     helper 'spree/products'
     respond_to :html
+    layout 'spree/layouts/crave'
 
     def index
       @searcher = build_searcher(params)
